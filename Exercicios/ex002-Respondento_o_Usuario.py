a71ef7d537380ade367c8d8fda517f1906b81b66
Alexandre Raminelli Junior <123258885+alexandreraminelli@users.noreply.github.com>
2024-01-13 19:12:15 -0300

    Módulo 1 Exercício 2

---
+++ Exercicios/ex002-Respondento_o_Usuario.py
@@ -0,0 +1,8 @@
+# Respondendo ao usuário
+
+nome = input('Qual é seu nome? ')
+
+#print('É um prazer te conhecer,', nome)
+
+#Usando saída formatada:
+print('É um prazer te conhecer {}!'.format(nome))
