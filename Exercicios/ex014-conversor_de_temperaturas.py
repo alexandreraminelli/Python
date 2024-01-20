dc55e778650edd5c41cee20c47f276775c429649
Alexandre Raminelli Junior <123258885+alexandreraminelli@users.noreply.github.com>
2024-01-20 17:07:53 -0300

    Módulo 1 Exercício 14

---
+++ Exercicios/ex014-conversor_de_temperaturas.py
@@ -0,0 +1,6 @@
+celsius = float(input('Digite a temperatura em °C: '))
+
+kelvin = celsius + 273
+fahrenheit = celsius * 1.8 + 32
+
+print('{}°C equivalem a {}°K e {}°F'.format(celsius, kelvin, fahrenheit))
