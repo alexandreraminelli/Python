8da3f9721a85a79f266bcc98ce06c892987fcea1
Alexandre Raminelli Junior <123258885+alexandreraminelli@users.noreply.github.com>
2024-01-22 10:55:51 -0300

    Módulo 1 Exercício 18

--- Exercicios/ex018-seno_cosseno_tangente.py
+++ Exercicios/ex018-seno_cosseno_tangente.py
@@ -1,12 +1,12 @@
 import math
-#VER RESOLUÇÃO
-angulo = float(input("Digite um angulo: "))
 
-#Converter angulo em radianos pro math calcular
+angulo = float(input("Digite um ângulo: "))
+
+# Converter angulo em radianos pro math calcular
 rad = math.radians(angulo)
 
-sen = math.sinh(rad)
-cos = math.cosh(rad)
-tan = math.tanh(rad)
+sen = math.sin(rad)
+cos = math.cos(rad)
+tan = math.tan(rad)
 
-print("O ângulo de {}° tem: \nSeno = {} \nCosseno = {} e \nTangente = {}".format(angulo, sen, cos, tan))
+print("O ângulo de {}° tem: \nSeno = {:.2f} \nCosseno = {:.2f} e \nTangente = {:.2f}".format(angulo, sen, cos, tan))
