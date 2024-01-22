3a08faa8b009631493812329af1b8f838da2acca
Alexandre Raminelli Junior <123258885+alexandreraminelli@users.noreply.github.com>
2024-01-22 17:59:57 -0300

    Módulo 1 Exercício 23

--- Exercicios/ex023-separando_digitos_de_numero.py
+++ Exercicios/ex023-separando_digitos_de_numero.py
@@ -1,6 +1,6 @@
-numero = input("Digite um número natural entre 0 e 9999: ")
+numero = int(input("Digite um número natural entre 0 e 9999: "))
 
-print("Unidade:", numero[3])
-print("Dezena:", numero[2])
-print("Centena:", numero[1])
-print("Milhar:", numero[0])
+print("Unidade:", numero // 1 % 10)
+print("Dezena:", numero // 10 % 10)
+print("Centena:", numero // 100 % 10)
+print("Milhar:", numero // 1000 % 10)
