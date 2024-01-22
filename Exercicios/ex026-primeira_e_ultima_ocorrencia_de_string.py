345249faf30018787b6d645413ef309771105274
Alexandre Raminelli Junior <123258885+alexandreraminelli@users.noreply.github.com>
2024-01-22 19:44:46 -0300

    Módulo 1 Exercício 26

--- Exercicios/ex026-primeira_e_ultima_ocorrencia_de_string.py
+++ Exercicios/ex026-primeira_e_ultima_ocorrencia_de_string.py
@@ -2,7 +2,7 @@ frase = input("Digite uma frase:\n")
 frase = frase.lower()
 
 quantidade = frase.count('a')
-primeira = frase.find('a')
-ultima = frase.rfind('a')
+primeira = frase.find('a') + 1
+ultima = frase.rfind('a') + 1
 
 print("Na sua frase, aparece a letra \"A\" {} vezes. Ela aparece pela primeira vez na {}ª posição e na última vez na {}ª posição.".format(quantidade, primeira, ultima))
