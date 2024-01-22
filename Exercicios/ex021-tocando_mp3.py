e4d7f6f2d91a3712da4850e73551105a34b8bfed
Alexandre Raminelli Junior <123258885+alexandreraminelli@users.noreply.github.com>
2024-01-22 11:32:38 -0300

    Módulo 1 Exercício 21

--- Exercicios/ex021-tocando_mp3.py
+++ Exercicios/ex021-tocando_mp3.py
@@ -1,1 +1,9 @@
-#VER RESOLUÇÃO
+import pygame
+
+pygame.init()
+
+pygame.mixer.music.load('ex021.mp3')
+pygame.mixer.music.play()
+
+# pygame.event.wait()
+terminar = input('Aperte enter para encerrar a música')
