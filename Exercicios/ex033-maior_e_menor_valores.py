459460732a140228c9d559aa763fdf679d84408e
Alexandre Raminelli Junior <123258885+alexandreraminelli@users.noreply.github.com>
2024-01-23 18:48:11 -0300

    Módulo 1 Exercício 33

--- Exercicios/ex033-maior_e_menor_valores.py
+++ Exercicios/ex033-maior_e_menor_valores.py
@@ -1,10 +1,24 @@
+from time import sleep
+
 num1 = int(input('Digite o 1º número: '))
 num2 = int(input('Digite o 2º número: '))
 num3 = int(input('Digite o 3º número: '))
 
+print('\nAnalisando os números...\n')
+sleep(1.5)
+
+# Maior
 if num1 > num2 and num1 > num3:
-    print('O primeiro número, {}, é o maior.'.format(num1))
+    print('O número {} é o maior.'.format(num1))
 if num2 > num1 and num2 > num3:
-    print('O segundo número, {}, é o maior.'.format(num2))
+    print('O número {} é o maior.'.format(num2))
 if num3 > num1 and num3 > num2:
-    print('O terceiro número, {}, é o maior.'.format(num3))
+    print('O número {} é o maior.'.format(num3))
+
+#Menor
+if num1 < num2 and num1 < num3:
+    print('O número {} é o menor.'.format(num1))
+if num2 < num1 and num2 < num3:
+    print('O número {} é o menor.'.format(num2))
+if num3 < num1 and num3 < num2:
+    print('O número {} é o menor'.format(num3))
