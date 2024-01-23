28ab385a3d2b01a0fad9164aac8a6cbeb30ed037
Alexandre Raminelli Junior <123258885+alexandreraminelli@users.noreply.github.com>
2024-01-23 18:18:42 -0300

    Módulo 1 Exercício 28

--- Exercicios/ex028-jogo_adivinhacao_v1.py
+++ Exercicios/ex028-jogo_adivinhacao_v1.py
@@ -1,9 +1,14 @@
 import random
+from time import sleep
 
 print('ADIVINHE O NÚMERO!!!')
 numero = random.randint(0,5)
 
 resposta = int(input('Em que número eu pensei (entre 0 e 5)?\n'))
+# efeito de tela de carregamento
+print('PROCESSANDO...')
+sleep(2)
+
 if numero == resposta:
     print('PARABÉNS!!! Você acertou!\nO número era {}'.format(numero))
 else:
