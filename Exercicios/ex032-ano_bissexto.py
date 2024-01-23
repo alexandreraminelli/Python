eb4a782c0b45628a1607284858a04799fdf9267d
Alexandre Raminelli Junior <123258885+alexandreraminelli@users.noreply.github.com>
2024-01-23 18:39:35 -0300

    Módulo 1 Exercício 32

--- Exercicios/ex032-ano_bissexto.py
+++ Exercicios/ex032-ano_bissexto.py
@@ -1,4 +1,13 @@
+from datetime import date
+from time import sleep
+
 ano = int(input('Digite um ano: '))
+
+if ano == 0:
+    ano = date.today().year
+
+print('Analisando o ano {}...'.format(ano))
+sleep(2)
 
 if ano % 4 == 0:
     if ano % 100 != 0:
