b86260e20838bc02b6babd8f922c8ea1ccf65df3
Alexandre Raminelli Junior <123258885+alexandreraminelli@users.noreply.github.com>
2024-01-25 19:47:53 -0300

    Módulo 2 Exercício 38

--- Exercicios/ex038-comparando_numeros.py
+++ Exercicios/ex038-comparando_numeros.py
@@ -4,6 +4,6 @@ num2 = int(input('Digite outro número: '))
 if num1 > num2:
     print("O primeiro valor, {}, é maior que o segundo, {}.".format(num1, num2))
 elif num1 < num2:
-    print('O segundo valor, {}, é maior que o segundo, {}.'.format(num2, num1))
+    print('O segundo valor, {}, é maior que o primeiro, {}.'.format(num2, num1))
 else:
     print('Ambos os valores são os mesmos: {}.'.format(num1))
