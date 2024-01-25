8ec12fbc3b4420eecc7f7d0f0309709614d0c4cd
Alexandre Raminelli Junior <123258885+alexandreraminelli@users.noreply.github.com>
2024-01-25 20:17:23 -0300

    Módulo 2 Exercício 42

--- Exercicios/ex042-analisando_triangulos_v2.py
+++ Exercicios/ex042-analisando_triangulos_v2.py
@@ -3,7 +3,7 @@ reta2 = float(input('Comprimento da 2ª reta: '))
 reta3 = float(input('Comprimento da 3ª reta: '))
 
 if reta1 + reta2 > reta3 and reta1 + reta3 > reta2 and reta2 + reta3 > reta1:
-    if reta1 == reta2 and reta1 == reta3:
+    if reta1 == reta2 == reta3:
         print('Essas 3 retas podem formar um triângulo equilátero.')
     elif reta1 == reta2 or reta1 == reta3 or reta2 == reta3:
         print('Essas 3 retas podem formar um triângulo isósceles')
