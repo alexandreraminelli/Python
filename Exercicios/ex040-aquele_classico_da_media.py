7afaeebf780e18e6ebe297e359cc1f8334885226
Alexandre Raminelli Junior <123258885+alexandreraminelli@users.noreply.github.com>
2024-01-25 19:54:36 -0300

    Módulo 2 Exercício 40

--- Exercicios/ex040-aquele_classico_da_media.py
+++ Exercicios/ex040-aquele_classico_da_media.py
@@ -2,9 +2,10 @@ nota1 = float(input('Informe a primeira nota: '))
 nota2 = float(input('Informe a segunda nota: '))
 
 media = (nota1 + nota2)/2
+print('Sua média é {}.'.format(media))
 if media < 5:
     print('REPROVADO')
-elif media > 5 and media < 6.9:
+elif 5 <= media < 6.9:
     print('RECUPERAÇÃO')
-else:
+elif media >= 7:
     print('APROVADO')
