3986c2c37dd5328c80030beef9adfa694fbd8876
Alexandre Raminelli Junior <123258885+alexandreraminelli@users.noreply.github.com>
2024-01-27 12:25:40 -0300

    Módulo 2 Aula 13 (Exercício 56)

---
+++ Exercicios/ex055-maior_e_menor_da_sequencia.py
@@ -0,0 +1,10 @@
+lista = []
+
+for ordem in range(1, 6):
+    peso = float(input('Qual é o peso da {}ª pessoa (em kg): '.format(ordem)))
+    lista.append(peso)
+
+lista.sort()  # Ordena os valores da lista do maior pro maior
+print(lista)
+
+print('O maior peso é {}kg e o menor peso é {}kg.'.format(lista[4], lista[0]))
