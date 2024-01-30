1814ab3d5a06eb6fb37004a29215fc3ef80d2655
Alexandre Raminelli Junior <123258885+alexandreraminelli@users.noreply.github.com>
2024-01-30 16:45:00 -0300

    Módulo 2 Exercício 52

--- Exercicios/ex052-numeros_primos.py
+++ Exercicios/ex052-numeros_primos.py
@@ -1,3 +1,19 @@
 numero = int(input("Digite um número inteiro: "))
 
-# VER RESOLUÇÃO
+divisores = 0
+for c in range(1, numero + 1):
+    if numero % c == 0:
+        print('\033[34m', end='')
+        divisores += 1
+    else:
+        print('\033[m', end='')
+    print(c, end=' ')
+print('\n\033[35m')
+
+print('O número {} tem {} divisores'.format(numero, divisores), end='')
+if divisores == 2:
+    print(': 1 e ele mesmo. \nEntão, ele É PRIMO!')
+else:
+    print('. \nPortanto, ele NÃO É PRIMO!')
+
+print('\033[m')
