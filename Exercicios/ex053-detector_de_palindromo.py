bf776dd57c59ecbda6eca6cb46d3223ba3d01b50
Alexandre Raminelli Junior <123258885+alexandreraminelli@users.noreply.github.com>
2024-01-30 17:40:42 -0300

    Módulo 2 Exercício 53

--- Exercicios/ex053-detector_de_palindromo.py
+++ Exercicios/ex053-detector_de_palindromo.py
@@ -1,3 +1,13 @@
-frase = str(input('Digite uma frase: '))
+frase = str(input('Digite uma frase (sem acentos): ')).strip().upper()
 
-# VER RESOLUÇÃO
+# Retirar espaços da frase
+palavras = frase.split()
+frase_junto = ''.join(palavras)
+
+frase_inversa = frase_junto[::-1]
+
+print('A frase {} invertida é {}.'.format(frase_junto, frase_inversa))
+if frase_junto == frase_inversa:
+    print('Essa frase é um palíndromo!')
+else:
+    print('Essa frase não é um palíndromo!')
