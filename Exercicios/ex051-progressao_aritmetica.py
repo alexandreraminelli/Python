505516044093c9d70daea38463330790e1f95c09
Alexandre Raminelli Junior <123258885+alexandreraminelli@users.noreply.github.com>
2024-01-30 16:14:08 -0300

    Módulo 2 Exercício 51

--- Exercicios/ex051-progressao_aritmetica.py
+++ Exercicios/ex051-progressao_aritmetica.py
@@ -3,4 +3,5 @@ razao = int(input('Digite a razão da PA: '))
 
 print('Os 10 primeiros termos dessa PA são: ')
 for termo in range(primeiro_termo, primeiro_termo + 10 * razao, razao):
-    print(termo)
+    print(termo, end=' → ')
+print('...')
