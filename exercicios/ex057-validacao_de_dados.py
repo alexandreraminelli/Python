aed8485050c03eca4eca19b3d0f12bd287b81b11
Alexandre Raminelli Junior <123258885+alexandreraminelli@users.noreply.github.com>
2024-02-14 08:14:34 -0300

    Módulo 2 Exercício 57

--- exercicios/ex057-validacao_de_dados.py
+++ exercicios/ex057-validacao_de_dados.py
@@ -1,6 +1,4 @@
-sexo = ''
+sexo = str(input('Informe seu sexo: [M/F]: ')).strip().upper()[0]
 while not (sexo == 'M' or sexo == 'F'):
-    sexo = str(input('Digite seu sexo [M/F]: ').upper())
-    if not (sexo == 'M' or sexo == 'F'):
-        print('Entrada inválida. Digite apenas M ou F!')
+    sexo = input('Entrada inválida. Informe seu sexo [M/F]: ').strip().upper()[0]
 print('Seu sexo inserido é {}.'.format(sexo))
